db9cc056cb2565a1034d4046af13b8da224f113d
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-02-17 16:05:51 -0300

    OrderItem, many-to-many association with extra attributes --PART2

---
+++ src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
@@ -0,0 +1,15 @@
+package com.educandoweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.course.entities.OrderItem;
+
+
+//Interface de repositório para a entidade OrderItem
+//Estende JpaRepository, que fornece métodos padrão para operações CRUD
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+	// Não é necessário declarar métodos básicos como findAll() ou findById(),
+	// pois o JpaRepository já fornece essas implementações automaticamente.
+
+}
