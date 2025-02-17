b2617620b59a5b3d3d98831767c1b318247f0ebb
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-02-17 19:21:00 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity // Indica que essa classe é uma entidade JPA, ou seja, será mapeada para uma
@@ -38,9 +40,21 @@ public class Order implements Serializable {
 	@JoinColumn(name = "client_id") // Especifica a chave estrangeira que referencia a entidade "User" no banco de
 									// dados
 	private User client;
-	
-	@OneToMany(mappedBy  = "id.order")
+
+	// Relacionamento um-para-muitos: Um pedido pode ter vários itens associados a
+	// ele.
+	// "mappedBy = id.order" indica que o mapeamento já foi feito dentro da entidade
+	// OrderItem.
+	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+
+	// Relacionamento um-para-um: Cada pedido possui um único pagamento.
+	// "mappedBy = order" significa que a chave estrangeira está na entidade
+	// Payment.
+	// "cascade = CascadeType.ALL" faz com que qualquer operação no pedido afete
+	// automaticamente o pagamento.
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 
 	public Order() {
 	}
@@ -87,9 +101,17 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
-	
-	public Set<OrderItem> getItems(){
+
+	public Set<OrderItem> getItems() {
 		return items;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	@Override
